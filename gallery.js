af25772716ab585e545034111a4b09a197e33ce6
Battals <batt0006@stud.kea.dk>
2022-12-05 13:14:44 +0100

    Rettet redudant kode

--- gallery.js
+++ gallery.js
@@ -1,56 +1,75 @@
-
 // Links in the gallery will be put into the table on page load
 //TODO: We need to not load everything at once, and also make it able to go 3 on an X axis, then continue down like that evt.
-const images = []
+const images = [];
 
-const overlayImg = document.getElementById('active-overlay-image');
-const overlay = document.getElementById('active-overlay');
+const overlayImg = document.getElementById("active-overlay-image");
+const overlay = document.getElementById("active-overlay");
 
 function displayImage(object) {
-    overlayImg.src = object.src
-    overlay.style.display = "block";
+  overlayImg.src = object.src;
+  overlay.style.display = "block";
 }
 
 function deactivateOverlay() {
-    overlay.style.display = "none";
-    overlayImg.src = ""
+  overlay.style.display = "none";
+  overlayImg.src = "";
+}
+
+function fetchApi() {
+  var myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+  var requestOptions = {
+    method: "get",
+    headers: myHeaders,
+    redirect: "follow",
+  };
+
+  //Deres URL
+  fetch(
+    "https://v1.nocodeapi.com/aps/instagram/pjCNBSyiUmzTFIzO",
+    requestOptions
+  )
+    .then((response) => response.json())
+    .then((result) => {
+      // Parse the JSON response and extract the photo URLs
+      const photoUrls = result.data.map((photo) => photo.media_url);
+
+      galleryStart(photoUrls);
+    });
 }
 
 function insertImages() {
-    let html = "";
-    var maxVal = 3;
-    var amount = 0;
-    while(amount < images.length){
-            html += `
+  let html = "";
+  var maxVal = 3;
+  var amount = 0;
+  while (amount < images.length) {
+    html += `
         <tr>
-        `
-            for (j = 0; j < maxVal; j++) {
-                html += `
+        `;
+    for (j = 0; j < maxVal; j++) {
+      html += `
                 <td><img onclick="displayImage(this)"
                 src="${images[amount]}"
                 class="img rounded"></img>
-                `
-                amount++
-                if(amount===images.length){
-                    break
-                }
-            }
-            html += `
+                `;
+      amount++;
+      if (amount === images.length) {
+        break;
+      }
+    }
+    html += `
         </tr>
-        `
-        }
-        document.querySelector('.image-data').innerHTML = html;
-    }
+        `;
+  }
+  document.querySelector(".image-data").innerHTML = html;
+}
 
- 
- 
-
-    function galleryStart(data) {
-        counter = 0;
-        while(counter < data.length){
-            console.log(data[counter])
-            images.push(data[counter])
-            counter++
-        }
-        insertImages()
-    }
+function galleryStart(data) {
+  counter = 0;
+  while (counter < data.length) {
+    console.log(data[counter]);
+    images.push(data[counter]);
+    counter++;
+  }
+  insertImages();
+}
