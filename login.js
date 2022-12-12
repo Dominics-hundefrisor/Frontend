bf15d4241cb0934450468f6363f8104bc917bf1c
Yaimee <37310514+Yaimee@users.noreply.github.com>
2022-12-12 19:26:34 +0100

    Rasmus

--- login.js
+++ login.js
@@ -17,11 +17,35 @@ function getInfo() {
     let username = document.getElementById("username").value;
     let password = document.getElementById("password").value;
 
-    for (let i = 0; i < objPeople.length; i++) {
+    let isSuccessful = postUserDetails(username,password);
+    if(!isSuccessful) {
+        document.getElementById("login-heading").innerHTML = "Incorrect username or password";
+    }
+    /*for (let i = 0; i < objPeople.length; i++) {
         if(username == objPeople[i].username && password == objPeople[i].password) {
             window.location.href = "index.html"
             return
         }
-    }
-    document.getElementById("login-heading").innerHTML = "Incorrect username or password"
+    }*/
 }
+
+const postUserDetails = async (username, password) => {
+    const url = 'http://localhost:8080/login';
+    const data = JSON.stringify({ username: username, password: password});
+    try {
+      const response = await fetch(url, {
+        method: "POST",
+        body: data,
+        headers: {
+            "Content-type": "application/json"
+          }
+      });
+      if (response.ok) {
+        console.log("access granted");
+        const jsonResponse = await response.json();
+      }
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  };
