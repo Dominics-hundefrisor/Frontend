8de9b793b1ae95a78a3e0571d3db1bba38e45797
Toby-byte <tobiasbogenielsen@gmail.com>
2022-12-13 16:01:49 +0100

    inner html og fetch til åbningstider

--- fetch-test.js
+++ fetch-test.js
@@ -4,7 +4,7 @@
     // remember to go back to http://localhost:8080 instead of webapp for testing locally
     // remember to go back to https://tobias-spring-projekt-service.azurewebsites.net instead of webapp for testing on server
 
-const api_url = 'http://localhost:8080/api/v1/price/findPriceById/';
+const api_url = 'http://localhost:8080/api/v1/price/';
 
 async function getPriceInfo (id,item) {
     const response = await fetch(api_url + id);
@@ -16,12 +16,12 @@ async function getPriceInfo (id,item) {
 
 getPriceInfo(1,"klip-1");
 getPriceInfo(2,"klip-2");
-getPriceInfo(3,"klip-3");
-getPriceInfo(4,"klip-4");
-getPriceInfo(5,"trimming-1");
-getPriceInfo(6,"Touch-up-1");
-getPriceInfo(7,"Touch-up-2");
-getPriceInfo(8,"Tillæg-1");
+getPriceInfo(4,"klip-3");
+getPriceInfo(5,"klip-4");
+getPriceInfo(6,"trimming-1");
+getPriceInfo(7,"Touch-up-1");
+getPriceInfo(8,"Touch-up-2");
+getPriceInfo(9,"Tillæg-1");
 
 // husk res.json skal skrives sådan her res.json()
 // fetch(api_url)
