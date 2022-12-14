a4e3de2157c7a09b2ea40a122d21b6ba64ebec22
Battals <batt0006@stud.kea.dk>
2022-12-14 13:22:12 +0100

    Adminpage

--- admin.js
+++ admin.js
@@ -1,2 +1,22 @@
 const div = document.getElementById("admin")
 const text = "test"
+
+localStorage.setItem("USERNAME", "KODE" )
+
+function updateInfo(){
+    const phone = "+45 52 60 10 56"
+}
+
+function updatePhone(){
+    console.log("test");
+}
+
+function updateEmail(){
+    console.log("test")
+}
+
+function updateAddress(){
+    console.log("test");
+}
+
+
