85e58d9af9efc0588808f38f95999d8d938f82bd
Battals <batt0006@stud.kea.dk>
2022-12-14 20:31:16 +0100

    Rettelser

--- login.js
+++ login.js
@@ -51,6 +51,7 @@ const putPostOrPatchValue = async (url, data, method) => {
         sessionStorage.setItem("token", JSON.stringify(token));
         sendReq("http://localhost:8080/hi");
         /*window.location("http://localhost:8080/hi");*/
+        /*
         console.log(jsonResponse);
       }
     } catch (error) {
@@ -74,4 +75,44 @@ const putPostOrPatchValue = async (url, data, method) => {
         .catch(err => {
             console.error(err.message);
         })
+}
+*/
+function checkLogin(){
+    event.preventDefault();
+
+    var username = "admin";
+    var pass = "admin123";
+
+
+    var user = {
+        username: username,
+        password: pass,
+    };
+
+    var json = JSON.stringify(user);
+
+    sessionStorage.setItem(username, json);
+
+    var input1 = document.getElementById("username1").value;
+    var input2 = document.getElementById("password").value;
+
+    var loginMessage = document.getElementById("loginMessage");
+
+
+    var user = sessionStorage.getItem(username);
+    var data = JSON.parse(user);
+
+    if(user == null){
+        console.log("fell");
+    }else if (input1 == data.username && input2 == data.password){
+        loginMessage.style.color = "green";
+        loginMessage.style.textAlign = "center";
+        loginMessage.innerHTML = "Succes";
+        document.location.pathname = "adminpage.html"
+    }else {
+        loginMessage.style.color = "red";
+        loginMessage.style.textAlign = "center";
+        loginMessage.innerHTML = "Forkert kode";
+    }
+
 }
