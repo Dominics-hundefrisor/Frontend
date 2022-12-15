bc18a43e0dd67c82aef26a88e649f4a45d69fe57
Battals <batt0006@stud.kea.dk>
2022-12-15 13:31:16 +0100

    CSS rettelser

--- admin.js
+++ admin.js
@@ -1,21 +1,29 @@
 
 function updatePhone(){
-
+let p = document.getElementById("phoneMessage");
     let phone = document.getElementById("phone").value;
     let phoneNumber = JSON.stringify({phoneNumber: phone});
     putPostOrPatchValue("http://localhost:8080/api/v1/contact/update-phone", phoneNumber, 'PUT');
+    p.style.color = "green"
+    p.innerHTML = "Telefon opdateret"
 }
 
 function updateEmail(){
+  let p = document.getElementById("emailMessage");
   let email = document.getElementById("email").value;
   let emails = JSON.stringify({email: email});
   putPostOrPatchValue("http://localhost:8080/api/v1/contact/update-email", emails, 'PUT');
+  p.style.color = "green"
+    p.innerHTML = "Email opdateret"
 }
 
 function updateAddress(){
+  let p = document.getElementById("addressMessage");
   let address = document.getElementById("adresse").value;
   let adresse = JSON.stringify({address: address});
   putPostOrPatchValue("http://localhost:8080/api/v1/contact/update-address", adresse, 'PUT');
+  p.style.color = "green"
+    p.innerHTML = "Adresse opdateret"
 }
 
 const putPostOrPatchValue = async (url, data, method) => {
